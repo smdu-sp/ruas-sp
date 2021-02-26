c8fea3d589de77db04755526bc09f7fe41e3b0a4
Renan <renan.scorp@gmail.com>
2021-02-26 16:13:06 -0300

    Redesign do frontend de acordo com a identidade visual definida por SMUL/ASCOM

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,13 +1,46 @@
 <template>
     <div class="container">
-        <div class="row">
-            <img src="/img/smul_logo_azul.png" class="ml-2" alt="SMUL" width="248" height="176">
-            <h1 style="line-height: 3">Projeto Ruas SP</h1>
+        <div id="header" class="row">
+            <div class="col-6">
+                <img src="/img/logo_ruassp.png" alt="Ruas SP" width="248" height="176">
+            </div>
+            <div class="col-6 text-right">
+                <img src="/img/logo_smul.png" class="mt-3" alt="SMUL">
+            </div>
+            <div id="link-bt-container">
+                <div class="col-4 text-center" v-for="link in links" v-bind:key="link.url">
+                    <!-- <div class="link-bt text-center">
+                        <a :href="link.url" target="_blank">{{link.nome}}</a>
+                    </div> -->
+                    <a :href="link.url" target="_blank"><button class="btn btn-secondary">{{ link.nome }}</button></a>
+                </div>
+            </div>
+            
+            <!-- <h1 style="line-height: 3">Projeto Ruas SP</h1> -->
         </div>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data() {
+            return {
+                links: [
+                    {
+                        nome: 'SOBRE O PROJETO',
+                        url: 'https://www.prefeitura.sp.gov.br/cidade/secretarias/licenciamento/noticias/index.php?p=308573'
+                    },
+                    {
+                        nome: 'CONFIRA AS VIAS APROVADAS',
+                        url: 'https://www.prefeitura.sp.gov.br/cidade/secretarias/licenciamento/servicos/index.php?p=308717'
+                    },
+                    {
+                        nome: 'MAIS INFORMAÇÕES',
+                        url: 'https://www.prefeitura.sp.gov.br/cidade/secretarias/licenciamento/servicos/index.php?p=308717'
+                    }
+                ]
+            }
+        }
+    }
 </script>
